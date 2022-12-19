bae1a7faa5b02a90c93e281bace77513290029d7
VincentHChoy <vhchoy@ualberta.ca>
2022-12-19 14:07:26 -0700

    adds basic counter functionalty

---
+++ src/components/Food.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  props: {
+  filename: String,
+  resource: Number
+  },
+  data(){
+    return{
+      counter: this.resource
+    }
+  },
+  methods:{
+    minus(num){
+      if(num === 0) return num
+      this.counter -= 1
+    },
+    getImageUrl(){
+      return new URL(`./icons/${this.filename}`, import.meta.url);
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="row">
+    <button min="0" @click="minus(counter)" class="counter">-</button>
+    <div class="resources">
+      <img width="40" height="40" :src="getImageUrl()" />
+      <p class="hidden-border">{{counter}}</p>
+    </div>
+    <button @click="counter++" class="counter">+</button>
+  </div>
+</template>
+
+<style scoped>
+.plus {
+  height: 40px;
+}
+
+.row {
+  width: 500px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.hidden-border{
+  border-style: none;
+}
+
+.counter{
+  cursor: pointer;
+}
+
+.resources {
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border-style: none;
+}
+
+
+.column {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/Resources.vue
@@ -0,0 +1,65 @@
+<script setup>
+import Food from "./Food.vue";
+import Resources from "./Food.vue";
+</script>
+
+
+<script>
+export default {
+data() {
+  return {
+    egg:0,
+    bug:0,
+    cherry:0,
+    wheat:0,
+    fish:0,
+    rat:0,
+  }
+}
+}
+</script>
+
+<template>
+  <section class="resources">
+    <Food filename="egg.png" :method="minus" :resource=egg />
+    <Food filename="bug.png" :resource="bug"/>
+    <Food filename="cherry.png" :resource="cherry"/>
+    <Food filename="wheat.png" :resource="wheat"/>
+    <Food filename="fish.png" :resource="fish"/>
+    <Food filename="rat.png" :resource="rat"/>
+  </section>
+  
+</template>
+
+
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.plus {
+  height: 40px;
+}
+
+.row {
+  width: 500px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.resources {
+  padding: 2rem;
+  color: blue;
+  background-color: cornsilk;
+  border: 5px double bisque;
+  text-align: center;
+}
+</style>
